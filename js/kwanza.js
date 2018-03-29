8162aa01b94bf1527af07daec15012d661c261a3
Romain Capelle <romain.capelle@gmail.com>
2018-03-29 13:55:00 +0200

    changement train and at sfdc

--- js/kwanza.js
+++ js/kwanza.js
@@ -1,20 +1,11 @@
-$('.fa-heart').click(function(event){
-    $(this).toggleClass('program-liked');
-});
-
-// function romain(event) {
-//     console.log("stuff");
-// }
-
-//console.log(HeartIcons);
-
-/*HeartIcons.forEach((HeartIcon) => {
-    HeartIcon.addEventListener('click', (event) => {
-        console.log("ta clicquer");
-        linkrow = event.currentTarget.querySelector('.icon-open-show-seance').parentNode.href;
-        console.log(linkrow);
-        parent = event.currentTarget.querySelector('.icon-open-show-seance').parentNode;
-        window.location.href = alinkrow;
-        HeartIcon.classList.toggle("program-liked");
-    });
-});*/
+function favoriteThisMovie(el) {
+    var el = $(el).children().first();
+    var alertEl = $('.alert-favorite');
+    el.toggleClass('program-liked');
+    if (el.hasClass("program-liked")) {
+        alertEl.removeClass('hidden');
+        window.setTimeout(function () {
+            alertEl.addClass('hidden');
+        }, 2000);
+    }
+}
